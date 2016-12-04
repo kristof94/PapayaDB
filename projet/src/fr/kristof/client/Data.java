128cc5e205283b5cb595c7b07aa32b0c6ccbd74f
master <master@master.home>
2016-12-04 14:25:06 +0100

    Documentation

--- projet/src/fr/kristof/client/Data.java
+++ projet/src/fr/kristof/client/Data.java
@@ -3,14 +3,22 @@
  */
 package fr.kristof.client;
 
-/**
+/** 
  * @author master
  *
  */
 public class Data {
 
+	/**
+	 * identifier ressource
+	 */
 	public String name;
 	
+	/**
+	 * Create data with name
+	 * @param name
+	 * Name use for identified ressource
+	 */
 	public Data(String name){
 		this.name = name;
 	}

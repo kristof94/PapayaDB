8eb56a8d18fcb8699c1b61a5e7f04f8f5b620249
master <master@master.home>
2016-12-04 14:29:25 +0100

    documentation

--- projet/src/fr/kristof/client/Query.java
+++ projet/src/fr/kristof/client/Query.java
@@ -4,6 +4,7 @@
 package fr.kristof.client;
 
 /**
+ * Object which contains Data and what the database must do.
  * @author master
  *
  */
@@ -12,15 +13,26 @@ public class Query {
 	private final String nameMethod;
 	private final String value;
 	
+	/**
+	 * Create a Query with method's name and value
+	 * @param nameMethod
+	 * @param value
+	 */
 	public Query(String nameMethod,String value){
 		this.nameMethod = nameMethod;
 		this.value = value;
 	}
 
+	/**
+	 * @return 
+	 */
 	public String getNameMethod() {
 		return nameMethod;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getValue() {
 		return value;
 	}

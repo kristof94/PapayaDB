8f6220050e05b10f6c14ae34a38c81d19c36a161
kristof94 <kristof94@users.noreply.github.com>
2016-12-09 12:47:04 +0100

    Update genKey.sh

--- projet/genKey.sh
+++ projet/genKey.sh
@@ -1,2 +1,1 @@
-keytool -genkey -keyalg RSA -alias cleprojet -keystore keystore.jks -storepass direct11 -validity 360 -keysize 2048
-
+keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -storepass tonmdp -validity 360 -keysize 2048 -ext san=ip:127.0.0.1
